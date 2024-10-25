98715dafecd78d638cd3e3939977ef1b6974cfa6
Caik Lima <124724369+Caik0@users.noreply.github.com>
2024-10-25 14:45:32 -0300

    src

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Bem-vindo ao Meu Site em React!</h1>
+      <p>Este é um site simples criado com React.js.</p>
+    </div>
+  );
+}
+
+export default App;
